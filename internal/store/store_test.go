1d87e71cdb757372a257fa352822a61ea276309a
The-iyed <iyedsebai@takiacademyteam.com>
2024-10-29 01:00:21 +0100

    add tests for delete key

--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -1,38 +1,48 @@
 package store
 
-
 import (
-  "testing"
-  "time"
+	"testing"
+	"time"
 )
 
+func TestSetAndGet(t *testing.T) {
 
-func TestSetAndGet(t *testing.T){
-    
-  kvStore := NewKeyValueStore()
+	kvStore := NewKeyValueStore()
 
-  kvStore.Set("key1", "value1", 5*time.Second)
-  
-  value , exist := kvStore.Get("key1")
-  if !exist || value != "value1" {
-    t.Errorf("Expected value1, got %s (exist: %v)", value, exist)
-  }
+	kvStore.Set("key1", "value1", 5*time.Second)
+
+	value, exist := kvStore.Get("key1")
+	if !exist || value != "value1" {
+		t.Errorf("Expected value1, got %s (exist: %v)", value, exist)
+	}
 
 }
 
+func TestExpireKey(t *testing.T) {
+	kvStore := NewKeyValueStore()
 
-func TestExpireKey(t *testing.T){
+	kvStore.Set("key1", "value1", 2*time.Second)
+
+	value, exist := kvStore.Get("key1")
+	if !exist || value != "value1" {
+		t.Errorf("Expected value1, got %s (exist: %v)", value, exist)
+	}
+	time.Sleep(4 * time.Second)
+	_, exist = kvStore.Get("key2")
+	if exist {
+		t.Errorf("Expected key2 to be expired")
+	}
+}
+
+func TestDeleteKey(t *testing.T) {
   kvStore := NewKeyValueStore()
 
-  kvStore.Set("key1","value1",2*time.Second)
-    
-  value , exist := kvStore.Get("key1")
-  if !exist || value != "value1" {
-    t.Errorf("Expected value1, got %s (exist: %v)", value, exist)
-  }
-  time.Sleep(4*time.Second)
-  _, exist := kvStore.Get("key2")
+  kvStore.Set("key3", "value3", 0) 
+
+  kvStore.Delete("key3")
+
+  _, exist := kvStore.Get("key3")
   if exist {
-    t.Errorf("Expected key2 to be expired")
+      t.Errorf("Expected key3 to be deleted")
   }
 }
