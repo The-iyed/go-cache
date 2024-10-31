77ea54888a53e4dfa1a686aa078fc5e253d4f9c7
The-iyed <iyedsebai@takiacademyteam.com>
2024-10-31 01:33:16 +0100

    add store methods to subscirbe and publish to rooms with a certain pattern

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -1,6 +1,7 @@
 package pubsub
 
 import (
+	"strings"
 	"sync"
 )
 
@@ -9,6 +10,7 @@ const BufferSize = 30
 type PubSub struct {
 	mu       sync.RWMutex
 	channels map[string][]chan string
+	patterns map[string][]chan string
 	buffers  map[string][]string
 }
 
@@ -16,6 +18,7 @@ func New() *PubSub {
 	return &PubSub{
 		channels: make(map[string][]chan string),
 		buffers:  make(map[string][]string),
+		patterns: make(map[string][]chan string),
 	}
 }
 
@@ -34,15 +37,35 @@ func (ps *PubSub) Subscribe(channel string) <-chan string {
 	return ch
 }
 
+func (ps *PubSub) SubscribePattern(pattern string) <-chan string {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
+	ch := make(chan string, 1)
+	ps.patterns[pattern] = append(ps.patterns[pattern], ch)
+	return ch
+}
+
 func (ps *PubSub) Publish(channel string, message string) {
 	ps.mu.RLock()
 	defer ps.mu.RUnlock()
-	
+
 	ps.storeInBuffer(channel, message)
 
 	if subscribers, ok := ps.channels[channel]; ok {
 		for _, subscriber := range subscribers {
-			subscriber <- message
+			go func(sub chan string) {
+				sub <- message
+			}(subscriber)
+		}
+	}
+
+	for pattern, subscribers := range ps.patterns {
+		if matchesPattern(pattern, channel) {
+			for _, subscriber := range subscribers {
+				go func(sub chan string) {
+					sub <- message
+				}(subscriber)
+			}
 		}
 	}
 }
@@ -57,6 +80,21 @@ func (ps *PubSub) Unsubscribe(channel string, ch <-chan string) {
 			ps.channels[channel] = append(subscribers[:i], subscribers[i+1:]...)
 			close(subscriber)
 			break
+		}
+	}
+}
+
+func (ps *PubSub) UnsubscribePattern(pattern string, ch <-chan string) {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
+
+	if subscribers, ok := ps.patterns[pattern]; ok {
+		for i, subscriber := range subscribers {
+			if subscriber == ch {
+				ps.patterns[pattern] = append(subscribers[:i], subscribers[i+1:]...)
+				close(subscriber)
+				break
+			}
 		}
 	}
 }
@@ -77,4 +115,27 @@ func (ps *PubSub) storeInBuffer(channel, message string) {
 		buffer = buffer[1:]
 	}
 	ps.buffers[channel] = buffer
+}
+
+func matchesPattern(pattern, channel string) bool {
+	if pattern == "*" {
+		return true
+	}
+	parts := strings.Split(pattern, "*")
+	if len(parts) == 1 {
+		return pattern == channel
+	}
+
+	for i, part := range parts {
+		if i == 0 && !strings.HasPrefix(channel, part) {
+			return false
+		}
+		if i == len(parts)-1 && !strings.HasSuffix(channel, part) {
+			return false
+		}
+		if !strings.Contains(channel, part) {
+			return false
+		}
+	}
+	return true
 }
